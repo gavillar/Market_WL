307049cbedee3b47dc2d2ed0a2a9af392dc86f03
BatataKente <danielpotoco@gmail.com>
2023-12-16 11:57:02 -0300

    implementação da tab bar com drawer

--- app/src/main/java/com/example/market_wl/layout/home/presenter/HomeActivity.java
+++ app/src/main/java/com/example/market_wl/layout/home/presenter/HomeActivity.java
@@ -1,17 +1,11 @@
 package com.example.market_wl.layout.home.presenter;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
-import com.example.market_wl.R;
 import com.example.market_wl.databinding.ActivityHomeBinding;
 import com.example.market_wl.layout.home.presenter.ui.home.HomeFragment;
-import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -31,6 +25,7 @@ public class HomeActivity extends AppCompatActivity {
                 )
                 .commitNow();
         }
+        getActivityHomeBinding().getRoot().open();
     }
 
     private ActivityHomeBinding getActivityHomeBinding() {
