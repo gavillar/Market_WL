207524aef2d6f6127c6cd4cc091ddeb9ee439d17
gavillar <gabriel_gomesvillar@hotmail.com>
2023-12-25 19:31:45 -0300

    Adicionando DataLogger no projeto

---
+++ app/src/main/java/com/example/market_wl/DataLogger/DataLogger.java
@@ -0,0 +1,35 @@
+package com.example.market_wl.DataLogger;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataLogger {
+
+    public static <T> void log(String path,
+                               int statusCode,
+                               Map<String, Object> headers,
+                               String error,
+                               Map<String, Object> params,
+                               Object response,
+                               T responseValue) {
+
+        Map<String, Object> logDictionary = new HashMap<>();
+        logDictionary.put("path", path);
+        logDictionary.put("status code", statusCode);
+        logDictionary.put("headers", headers);
+        logDictionary.put("message", error);
+        logDictionary.put("params", params);
+        logDictionary.put("response", response);
+        logDictionary.put("responseValue", responseValue);
+
+        System.out.println(logDictionary);
+    }
+
+    public static void logEvent(String title, String message) {
+        Map<String, Object> log = new HashMap<>();
+        log.put("title", title);
+        log.put("message", message);
+
+        System.out.println(log);
+    }
+}
