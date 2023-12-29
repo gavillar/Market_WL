073875ca8fbc1b1315fd0aa2d4b468a1bab62774
gavillar <gabriel_gomesvillar@hotmail.com>
2023-12-29 00:46:55 -0300

    Implementação do carousel na tela home

--- app/src/main/java/com/example/market_wl/layout/home/presenter/HomeActivity.java
+++ app/src/main/java/com/example/market_wl/layout/home/presenter/HomeActivity.java
@@ -1,20 +1,29 @@
 package com.example.market_wl.layout.home.presenter;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.market_wl.R;
 import com.example.market_wl.databinding.ActivityHomeBinding;
 import com.example.market_wl.extensions.AppCompatActivityExtended;
+import com.example.market_wl.layout.home.presenter.carousel.SlideItem;
+import com.example.market_wl.layout.home.presenter.carousel.SliderAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivityExtended {
 
     private ActivityHomeBinding activityHomeBinding;
 
+    private ViewPager2 viewPager2;
 
 
     @Override
@@ -25,6 +34,22 @@ public class HomeActivity extends AppCompatActivityExtended {
         setContentView(getActivityHomeBinding().getRoot());
         getHomeFragmentContainerView();
         getActivityHomeBindingRoot();
+        configCarousel();
+    }
+
+    void configCarousel() {
+
+        viewPager2 = findViewById(R.id.view_pager_carousel);
+
+        List<SlideItem> slideItem = new ArrayList<>();
+        slideItem.add(new SlideItem(R.drawable.image_offer));
+        slideItem.add(new SlideItem(R.drawable.image_offer));
+        slideItem.add(new SlideItem(R.drawable.image_offer));
+        slideItem.add(new SlideItem(R.drawable.image_offer));
+        slideItem.add(new SlideItem(R.drawable.image_offer));
+
+        viewPager2.setAdapter(new SliderAdapter(slideItem, viewPager2));
+
     }
 
 
---
+++ app/src/main/java/com/example/market_wl/layout/home/presenter/carousel/Carousel.java
@@ -0,0 +1,4 @@
+package com.example.market_wl.layout.home.presenter.carousel;
+
+public class Carousel {
+}
