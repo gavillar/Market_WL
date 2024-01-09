6e30ce4ca60e0dbe10e580383b66f4768686e16a
DJunqueiraO <danielpotoco@gmail.com>
2024-01-09 20:49:35 -0300

    ajustes na fragment, adição do campo de preenchuimento e da label

--- app/src/main/java/com/example/market_wl/layout/forgot_password/presenter/fragments/ForgotPasswordFragment.java
+++ app/src/main/java/com/example/market_wl/layout/forgot_password/presenter/fragments/ForgotPasswordFragment.java
@@ -9,24 +9,53 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.market_wl.R;
+import com.example.market_wl.components.EditTextComponent;
+import com.example.market_wl.components.TextViewComponent;
 import com.example.market_wl.databinding.FragmentForgotPasswordBinding;
 
 public class ForgotPasswordFragment extends Fragment {
 
+
+    private EditTextComponent forgotPasswordEditText;
+    private TextViewComponent forgotPasswordTextView;
     private FragmentForgotPasswordBinding fragmentForgotPasswordBinding;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        setForgotPasswordTextView();
+        setForgotPasswordEditText();
         return getFragmentForgotPasswordBinding().getRoot();
     }
 
-    public FragmentForgotPasswordBinding getFragmentForgotPasswordBinding() {
+    private FragmentForgotPasswordBinding getFragmentForgotPasswordBinding() {
         if(fragmentForgotPasswordBinding == null) {
             fragmentForgotPasswordBinding = (
                 FragmentForgotPasswordBinding.inflate(getLayoutInflater())
             );
         }
         return fragmentForgotPasswordBinding;
+    }
+    private void setForgotPasswordTextView() {
+        if(forgotPasswordTextView == null) {
+            forgotPasswordTextView = (
+                    getFragmentForgotPasswordBinding().forgotPasswordTextView
+            );
+            forgotPasswordTextView.setText(
+                    getResources().getText(R.string.qual_o_seu_email)
+            );
+        }
+    }
+
+    private void setForgotPasswordEditText() {
+        if(forgotPasswordEditText == null) {
+            forgotPasswordEditText = (
+                    getFragmentForgotPasswordBinding().forgotPasswordEditText
+            );
+            forgotPasswordEditText.setHint(
+                    getResources().getText(R.string.email)
+            );
+        }
     }
 }
